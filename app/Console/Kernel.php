54c081bc3a84b1e70a04aecf380dc7564e3c885f
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2018-12-22 22:35:08 +1000

    Adding getvideos command to the kernal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\GetVideos::class
     ];
 
     /**
@@ -24,6 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        // $schedule->call(function () {
+        //     dispatch(new App\Jobs\GetYoutubeVideos());
+        // })->everyMinute();
         // $schedule->command('inspire')
         //          ->hourly();
     }
