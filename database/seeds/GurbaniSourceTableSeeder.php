3699a19a77a52938420686c0203ecd96d5db8d24
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2019-01-10 22:01:27 +1000

    Adding seeding for gurbani source table

---
+++ database/seeds/GurbaniSourceTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GurbaniSourceTableSeeder extends Seeder
+{
+    private $data = [
+        [
+            'identifier' => 'G',
+            'punjabi' => 'sRI gurU gRMQ swihb jI',
+            'unicode' => 'ਸ੍ਰੀ ਗੁਰੂ ਗ੍ਰੰਥ ਸਾਹਿਬ ਜੀ',
+            'english' => 'Sri Guru Granth Sahib Ji',
+            'angs' => 1430
+        ],
+        [
+            'identifier' => 'D',
+            'punjabi' => 'sRI dsm gRMQ',
+            'unicode' => 'ਸ੍ਰੀ ਦਸਮ ਗ੍ਰੰਥ',
+            'english' => 'Sri Dasam Granth',
+            'angs' => 2820
+        ],
+        [
+            'identifier' => 'B',
+            'punjabi' => 'vwrW',
+            'unicode' => 'ਵਾਰਾਂ',
+            'english' => 'Vaaran',
+            'angs' => 42
+        ],
+        [
+            'identifier' => 'N',
+            'punjabi' => 'vwrW',
+            'unicode' => 'ਵਾਰਾਂ',
+            'english' => 'Vaaran',
+            'angs' => 1
+        ]
+    ];
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach ($this->data as $row) {
+            DB::table('gurbani_sources')->insert($row);
+        }
+    }
+}
