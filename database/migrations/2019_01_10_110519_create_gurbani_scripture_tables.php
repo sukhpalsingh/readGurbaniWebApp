5ac496105f287afed80a649035f8e09c2befb751
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2019-01-10 22:01:14 +1000

    Adding tables for gurbani scripture

---
+++ database/migrations/2019_01_10_110519_create_gurbani_scripture_tables.php
@@ -0,0 +1,76 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGurbaniScriptureTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gurbani_sources', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('identifier');
+            $table->string('punjabi')->nullable();
+            $table->string('unicode')->nullable();
+            $table->string('english')->nullable();
+            $table->integer('angs');
+        });
+
+        Schema::create('gurbani_writers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('punjabi');
+            $table->string('unicode');
+            $table->string('english');
+        });
+
+        Schema::create('gurbani_raags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('punjabi');
+            $table->string('unicode');
+            $table->string('english');
+            $table->integer('ang_from');
+            $table->integer('ang_to');
+            $table->string('info_english');
+        });
+
+        Schema::create('gurbani_scriptures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('gurmukhi');
+            $table->string('gurmukhi_unicode');
+            $table->string('punjabi')->nullable();
+            $table->string('punjabi_unicode')->nullable();
+            $table->string('devanagari')->nullable();
+            $table->string('devanagari_unicode')->nullable();
+            $table->string('english')->nullable();
+            $table->string('spanish')->nullable();
+            $table->string('transliteration');
+            $table->string('first_letters');
+            $table->string('first_letters_unicode');
+            $table->integer('ang');
+            $table->integer('pankti');
+            $table->unsignedInteger('gurbani_source_id');
+            $table->unsignedInteger('gurbani_raag_id')->nullable();
+            $table->unsignedInteger('gurbani_writer_id')->nullable();
+
+            $table->foreign('gurbani_source_id')->references('id')->on('gurbani_sources');
+            $table->foreign('gurbani_raag_id')->references('id')->on('gurbani_raags');
+            $table->foreign('gurbani_writer_id')->references('id')->on('gurbani_writers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
