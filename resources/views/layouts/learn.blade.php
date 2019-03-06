d8eaf8e69b0e39bb20b0b250767c1fae5518b532
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2019-03-06 21:13:57 +1000

    Updating learn layout

--- resources/views/layouts/learn.blade.php
+++ resources/views/layouts/learn.blade.php
@@ -7,9 +7,9 @@
         <div class="sidebar-logo">
             <img src="/images/logo.png" style="height: 40px;" /> <span style="font-weight: bold; font-size: 20px;">Gurbani</span>
         </div>
-        <ul class="sidebar-menu">
+        <ul class="sidebar-menu mt-3">
             <li class="nav-item">
-                <a class="nav-link active" href="/training-manager/courses"><i class="fas fa-graduation-cap"></i> Courses</a>
+                <a class="nav-link active" href="/training-manager/courses"><i class="fas fa-graduation-cap"></i> COURSES</a>
             </li>
         </ul>
     </div>
