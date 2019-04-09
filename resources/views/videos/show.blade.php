1af308af242fa44003c5a1efc11bcc93a450d9c2
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2019-04-09 21:33:12 +1000

    Adding autoplay and more info to the video details

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -5,17 +5,28 @@
 <div class="container-fluid">
     <div class="row mt-4">
         <div class="col-md-8 pl-4">
-            <iframe
-                class="w-100"
-                height="400px"
-                src="https://www.youtube.com/embed/{{$video->video_id}}"
-                frameborder="0" 
-                allowfullscreen
-            >
-            </iframe>
-            <p>{{$video->title}}</p>
+            <div class="row">
+                <div class="col-md-12">
+                    <iframe
+                        class="w-100"
+                        height="400px"
+                        src="https://www.youtube.com/embed/{{$video->video_id}}?autoplay=1"
+                        frameborder="0" 
+                        allowfullscreen
+                    >
+                    </iframe>
+                </div>
+            </div>
+            <h5 class="mt-3">{{$video->title}}</h5>
+            <p>
+                @if ($video->views > 0)
+                    {{ number_format($video->views) }} views
+                @else
+                    No views
+                @endif
+            </p>
+            <h6>{{ $video->channel_title }}</h6>
             <p class="text-muted">{{$video->description}}</p>
-            
         </div>
         <div class="col-md-4">
         </div>
