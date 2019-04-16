0429076d6f264134fcf91c6997bf75b8d26b8b88
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2019-04-16 21:51:30 +1000

    Importing serial data for sundar gutka scripture table

--- database/seeds/SundarGutkaScriptureTableSeeder.php
+++ database/seeds/SundarGutkaScriptureTableSeeder.php
@@ -6,9 +6,9 @@ use App\SundarGutkaScripture;
 class SundarGutkaScriptureTableSeeder extends Seeder
 {
     private $sql = "
-        INSERT INTO sundar_gutka_scriptures (sundar_gutka_id, gurmukhi, gurmukhi_unicode, translation_punjabi, translation_punjabi_unicode, translation_english, translation_spanish, transliteration_english, transliteration_devanagari, first_letters, first_letters_unicode, ang, pankti, shabad_id, gurbani_source_id, gurbani_raag_id, gurbani_writer_id)
+        INSERT INTO sundar_gutka_scriptures (sundar_gutka_id, gurmukhi, gurmukhi_unicode, translation_punjabi, translation_punjabi_unicode, translation_english, translation_spanish, transliteration_english, transliteration_devanagari, first_letters, first_letters_unicode, ang, pankti, shabad_id, gurbani_source_id, gurbani_raag_id, gurbani_writer_id, serial)
 
-        SELECT sundar_gutka_id, gurmukhi, gurmukhi_unicode, translation_punjabi, translation_punjabi_unicode, translation_english, translation_spanish, transliteration_english, transliteration_devanagari, first_letters, first_letters_unicode, ang, pankti, shabad_id, gurbani_source_id, gurbani_raag_id, gurbani_writer_id
+        SELECT sundar_gutka_id, gurmukhi, gurmukhi_unicode, translation_punjabi, translation_punjabi_unicode, translation_english, translation_spanish, transliteration_english, transliteration_devanagari, first_letters, first_letters_unicode, ang, pankti, shabad_id, gurbani_source_id, gurbani_raag_id, gurbani_writer_id, serial
         FROM
         (
             (
