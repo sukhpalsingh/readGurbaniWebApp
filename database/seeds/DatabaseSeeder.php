ae37b6804db09a38f52d41abb84f09425d98d5c0
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2019-06-30 12:54:43 +1000

    Adding default admin user seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UserTableSeeder::class);
         $this->call(GurbaniSourceTableSeeder::class);
         $this->call(SundarGutkaTableSeeder::class);
         $this->call(SundarGutkaScriptureTableSeeder::class);
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@readgurbani.com',
+            'password' => bcrypt('admin-default-password'),
+        ]);
+    }
+}
