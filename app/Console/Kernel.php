e34c2c5038963f1e14577d579926a5642720ac72
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2019-07-07 12:53:22 +1000

    Fixing the TagVideoService not found issue

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,7 +34,7 @@ class Kernel extends ConsoleKernel
         })->everyMinute();
 
         $schedule->call(function() {
-            (new App\Services\TagVideoService())->tagVideos();
+            (new TagVideoService())->tagVideos();
         })->everyMinute();
 
         // $schedule->command('inspire')
