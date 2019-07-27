c1598e565423914b0c4dabe540a74b1269b2afd4
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2019-07-27 12:15:50 +1000

    Updating fillable for the diskalbum model class

--- app/DiskAlbum.php
+++ app/DiskAlbum.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class DiskAlbum extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name_pan',
+        'name_eng',
+        'disk_category_id',
+        'disk_genre_id',
+        'serial',
+    ];
 }
