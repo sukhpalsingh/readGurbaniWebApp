624c8a99fdc3481d6dc4fa04fda77537f88c0e22
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2019-11-16 10:11:42 +1000

    adding index to videos table

---
+++ database/migrations/2019_11_16_000910_create_videos_index.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideosIndex extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('videos', function (Blueprint $table) {
+            $table->index(['video_id', 'channel_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('videos', function (Blueprint $table) {
+            $table->dropIndex('videos_video_id_channel_id_index');
+        });
+    }
+}
